67d0619e94810bc12a84075f6c55655a009822ad
Luan Martini <luancarlosmartini@gmail.com>
2026-01-05 20:33:57 -0300

    feat(frontend):Página de Login

---
+++ lib/views/pages/LoginPage.dart
@@ -0,0 +1,55 @@
+import "package:flutter/material.dart";
+
+class TelaLogin extends StatelessWidget {
+  const TelaLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Para Fazer"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 360,
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('E-mail'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 360,
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '');
+              },
+              child: const Text('Entrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
